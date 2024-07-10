45e35923ce3b13627145d7a6fad8b8f75d46ade0
Leo Folsom <leoebfolsom@gmail.com>
2024-07-10 00:00:13 -0700

    dynamically drop relevant tables before recreating in stored proc

--- Create__ElectricVehicles_Practical_EV_Zips.sql
+++ Create__ElectricVehicles_Practical_EV_Zips.sql
@@ -1,19 +1,17 @@
-DROP TABLE IF EXISTS core.dbo.ElectricVehicles_Practical_EV_Zips;
-DROP TABLE IF EXISTS testing.dbo.ElectricVehicles_Practical_EV_Zips;
-
 CREATE OR ALTER PROCEDURE Create__ElectricVehicles_Practical_EV_Zips
 @ExcludeMake nvarchar(30), @MinCountStar INT, @database nvarchar(30) AS
 BEGIN 
 	SET NOCOUNT ON;
 	DECLARE @Sql NVARCHAR(MAX);
-	SET @Sql = N'SELECT
-					PostalCode,
-					SUM(CASE WHEN ElectricRange > 200 THEN 1.0 ELSE 0.0 END)/COUNT(*) AS Practical_EV_Rate,
-					COUNT(*) as NumEVs
-				INTO ' + QUOTENAME(@database) + '.dbo.ElectricVehicles_Practical_EV_Zips 
-				FROM ' + QUOTENAME(@database) + '.dbo.ElectricVehicles 
-				WHERE Make != @ExcludeMake GROUP BY PostalCode
-				HAVING COUNT(*) > @MinCountStar';
+	SET @Sql = N'DROP TABLE IF EXISTS ' + QUOTENAME(@database) + '.dbo.ElectricVehicles_Practical_EV_Zips;
+		SELECT
+			PostalCode,
+			SUM(CASE WHEN ElectricRange > 200 THEN 1.0 ELSE 0.0 END)/COUNT(*) AS Practical_EV_Rate,
+			COUNT(*) as NumEVs
+		INTO ' + QUOTENAME(@database) + '.dbo.ElectricVehicles_Practical_EV_Zips 
+		FROM ' + QUOTENAME(@database) + '.dbo.ElectricVehicles 
+		WHERE Make != @ExcludeMake GROUP BY PostalCode
+		HAVING COUNT(*) > @MinCountStar';
  	EXECUTE sp_executesql @Sql, N'@ExcludeMake nvarchar(30), @MinCountStar INT', @ExcludeMake, @MinCountStar;
  end;
 
