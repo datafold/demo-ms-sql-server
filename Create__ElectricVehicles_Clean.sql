21ad922fc43c7fabaf7d0f9537f79cf711bc6778
Leo Folsom <leoebfolsom@gmail.com>
2024-07-10 11:08:23 -0700

    Update Create__ElectricVehicles_Clean.sql

--- Create__ElectricVehicles_Clean.sql
+++ Create__ElectricVehicles_Clean.sql
@@ -5,14 +5,30 @@ BEGIN
 	DECLARE @Sql NVARCHAR(MAX);
 	SET @Sql = N'DROP TABLE IF EXISTS ' + QUOTENAME(@database) + '.dbo.ElectricVehicles_Clean;
 				SELECT
-					VIN, County, City, State, PostalCode, ModelYear, Make, Model, ElectricVehicleType, 
-					CAFVEligibility, ElectricRange, BaseMSRP, LegislativeDistrict, DOLVehicleID, VehicleLocation,
-					ElectricUtility, CensusTract
+					VIN, 
+					County, 
+					City, 
+					State, 
+					PostalCode, 
+					REPLACE(ModelYear, '','', '''') as ModelYear, 
+					Make, 
+					REPLACE(Model, '' '', '''') as Model, 
+					ElectricVehicleType, 
+					CAFVEligibility, 
+					ElectricRange, 
+					BaseMSRP, 
+					LegislativeDistrict, 
+					DOLVehicleID, 
+					VehicleLocation,
+					ElectricUtility, 
+					CensusTract
 				INTO ' + QUOTENAME(@database) + '.dbo.ElectricVehicles_Clean
 				FROM ' + QUOTENAME(@database) + '.dbo.ElectricVehicles 
-				WHERE 1=1 @ExcludeClause';
+				WHERE 1=1 ' + @ExcludeClause;
  	EXECUTE sp_executesql @Sql, N'@ExcludeClause nvarchar(30)', @ExcludeClause;
  end;
 
--- EXEC Create__ElectricVehicles_Clean @ExcludeClause = 'AND TRUE', @database = 'testing';
-
+-- EXAMPLE
+-- ---------------------------------------------------------------
+-- EXEC Create__ElectricVehicles_Clean @ExcludeClause = 'AND Make != ''MINI''', @database = 'testing';
+-- SELECT * FROM testing.dbo.ElectricVehicles_Clean ORDER BY 2 DESC; 
