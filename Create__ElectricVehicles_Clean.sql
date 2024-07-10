1668f441efc1424a9b9ec1db7f2c07bdac859b3b
Leo Folsom <leoebfolsom@gmail.com>
2024-07-10 14:26:16 -0700

    Update Create__ElectricVehicles_Clean.sql

--- Create__ElectricVehicles_Clean.sql
+++ Create__ElectricVehicles_Clean.sql
@@ -9,7 +9,7 @@ BEGIN
 					County, 
 					City, 
 					State, 
-					12345 as PostalCode, 
+					22345 as PostalCode, 
 					REPLACE(ModelYear, '','', '''') as ModelYear, 
 					Make, 
 					Model, 
